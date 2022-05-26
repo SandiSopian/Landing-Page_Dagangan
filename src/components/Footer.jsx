18af525561557d03c7c233fd3925de9c5e50d4f7
Sandi Sopian <shunasand73@gmail.com>
2022-05-27 01:11:47 +0700

    Fix Query

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import InstagramIcon from "@material-ui/icons/Instagram";
+import WhatsAppIcon from "@material-ui/icons/WhatsApp";
 import "../styles/Footer.css";
 
 function Footer() {
   return (
     <div className="footer">
+      <p> Hubungi Kami:</p>
       <div className="socialMedia">
         <FacebookIcon />
         <InstagramIcon />
+        <WhatsAppIcon />
       </div>
       <p> &copy; 2022 Best Meat</p>
     </div>
